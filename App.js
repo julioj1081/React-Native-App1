dff7037eb0138e4a0c444e64234199ced6c5a935
Julio Alberto <julioj1081@hotmail.com>
2021-07-10 18:44:01 -0500

    Se agrega imagen y boton

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
 import React from "react";
-import { Text, View, StyleSheet, Image, Button } from "react-native";
+import { Text, View, StyleSheet, Image, Button, Alert } from "react-native";
 import img from "./assets/imagen.jpg";
 const App = () => {
   return (
     <View style={styles.vista}>
       <Text style={styles.titulo}>Hello word !</Text>
+      <Text style={styles.nombre}>Julio alberto</Text>
       <Image style={styles.imagen} source={img}></Image>
       <Button
-        //onPress={onclick}
-        onPress={() => console.log("click")}
-        title="Learn More.."
-        color="#841584"
-        accessibilityLabel="Learn more about this purple button"
+        onPress={onclick}
+        //onPress={() => Alert.alert("hello word")}
+        title="Wellcome"
+        style={styles.boton_learn}
       />
     </View>
   );
@@ -24,9 +24,21 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "white",
+    height: 100,
   },
-  titulo: { lineHeight: 100, color: "black", fontSize: 25 },
-  imagen: { width: 200, height: 200, borderRadius: 100 },
+  titulo: { lineHeight: 80, color: "black", fontSize: 25 },
+  nombre: { color: "gray", marginBottom: 20 },
+  imagen: {
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+    marginBottom: 40,
+  },
+  boton_learn: { backgroundColor: "#000", color: "red" },
 });
+
+function onclick() {
+  alert("hello word!");
+}
 
 export default App;
